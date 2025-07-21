2cd2aef0ab498a28ad7d083b7ca292ee066927e4
simetrio <simetrio@yandex.com>
2025-07-21 15:20:03 +0500

    Fix scheduler

--- cli/src/scheduler.ts
+++ cli/src/scheduler.ts
@@ -2,32 +2,48 @@ import Enumerable from "linq"
 
 async function work(action: () => Promise<void>, ...times: number[]) {
     if (!times.length) {
+        log("Times not found. Process only once", times)
         await action()
         return
     }
 
+    log("Start process by times", times)
+
     let lastTime = 0
-    const getTime = (hour: number) => new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), hour).getTime()
+    const getTime = (hour: number) => {
+        const now = new Date()
+        return now.getFullYear() * 1000000 + (now.getMonth() + 1) * 10000 + now.getDate() * 100 + hour
+    }
 
-    setInterval(() => {
-        const nextTime = Enumerable.from(times)
-            .select(getTime)
-            .where(x => x > lastTime)
-            .orderBy(x => x)
-            .firstOrDefault()
+    const check = async () => {
+        {
+            const now = getTime(new Date().getHours())
+            const nextTime = Enumerable.from(times)
+                .select(x => getTime(x))
+                .singleOrDefault(x => x > lastTime && x === now)
 
-        if (!nextTime) {
-            return
+            if (!nextTime) {
+                log(
+                    "Not found time to work",
+                    lastTime,
+                    now,
+                    Enumerable.from(times).select(x => getTime(x)).toArray()
+                )
+                return
+            }
+
+            log("Execute action", lastTime, now, nextTime)
+            lastTime = nextTime
+            await action()
         }
+    }
 
-        if (new Date().getTime() < nextTime) {
-            return
-        }
+    setInterval(() => check(), 5 * 60 * 1000)
+    await check()
+}
 
-        console.log(new Date(), "Execute action", lastTime, nextTime)
-        lastTime = nextTime
-        action()
-    }, 5 * 1000)
+function log(...parameters: any[]) {
+    console.log(new Date(), "Scheduler:", ...parameters)
 }
 
 export const scheduler = {
